93aebdad8f904ec440daa75dc024937c7fae7afa
CdmMandalorian <68617257+CdmMandalorian@users.noreply.github.com>
2020-12-30 20:48:56 -0800

    mode node_modules, .gitignore, README.md, package.json, and package-lock.jason all moved into the root folder

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactcms");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/cattalog");
 
 // Start the API server
 app.listen(PORT, function() {
