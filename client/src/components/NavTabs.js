9382f6e61d2bdf2def49b4af9648731bdf396c3d
Giovanny Franco <gfranco19@live.com>
2021-01-06 22:10:11 -0800

    navbar

--- client/src/components/NavTabs.js
+++ client/src/components/NavTabs.js
@@ -9,10 +9,10 @@ function NavTabs() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
-  <div className="collapse navbar-collapse" id="navbarNav">
+  <div className="collapse navbar-collapse" id="navbarNav"> */}
     <ul className="navbar-nav">
       <li className="nav-item active">
       <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
@@ -44,7 +44,7 @@ function NavTabs() {
         </Link>
       </li>
     </ul>
-  </div>
+  {/* </div> */}
 </nav>
   );
 }
