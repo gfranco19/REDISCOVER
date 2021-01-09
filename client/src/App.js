0c3716b4729fead81f799469ad425d1e62dc368a
shay_daniel <shay@isom.org>
2021-01-09 12:50:00 -0800

    styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,28 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Haunted from "./pages/HauntedPlaces/Haunted";
-import Historical from "./pages/HistoricalPlaces/Historical";
-import Film from "./pages/TvFilmPlaces/Film";
-import Welcome from "./welcomePage/Welcome";
+// import NavTabs from "./components/NavTabs/NavTabs";
+import Haunted from "./pages/Haunted/Haunted";
+import Historical from "./pages/Historical/Historical";
+import Film from "./pages/Film/Film";
+import Welcome from "./pages/welcomePage/Welcome";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import Example from "./pages/Example/Example"
+
 
 function App() {
   return (   
   <Router>
     <div>
-    <NavTabs />
+    {/* <NavTabs /> */}
     <Route exact path = "/" component ={Welcome} />
     <Route path = "/haunted" component ={Haunted} />
     <Route path = "/historical" component ={Historical} />
     <Route path = "/film" component ={Film} />
+    <Route path = "/Login" component ={Login} />
+    <Route path = "/Register" component ={Register} />
+    <Route path = "/Example" component ={Example} />
+
     {/* possible favorites page */}
     {/* <Route exact path = "/favorites" component ={Favorite} /> */}
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-<<<<<<< HEAD
-
-=======
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> 95f6ea116ee14c38cf7dcd99bc36ae2b7ffe8ae5
-
 
 ReactDOM.render(<App />, document.getElementById('root'));
