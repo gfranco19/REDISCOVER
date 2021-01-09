f528a1dd4347dae8e59577db5cfc8d487beec32e
Todd Murdoch <TheClash090@gmail.com>
2021-01-09 15:46:40 -0800

    completed the upload function to call the back and send to MONGO, TODO: finish Posts page to save images when refreshed etc.

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -5,3 +5,6 @@ import axios from 'axios';
 const url = 'http://localhost:5000/posts';
 
 export const fetchPosts = () => axios.get(url);
+
+// api request with cb function for entire post to specify url and data sent which is the entire new post //  
+export const createPost = (newPost) => axios.post(url, newPost)
