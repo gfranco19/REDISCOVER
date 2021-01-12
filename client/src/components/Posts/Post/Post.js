e3a9beeb1a3f22385d5b25e1db81aacd2220d446
Todd Murdoch <TheClash090@gmail.com>
2021-01-11 20:43:07 -0800

    Fixed Card functions to generate photo from user uploads

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import useStyles from './styles';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+import moment from 'moment';
+// import { useDispatch } from 'react-redux';
 
 
 
-const Post = () => {
+const Post = ({post}) => {
     const classes = useStyles();
     return(
-        <h1>more posts</h1>
+        <Card className={classes.card}>
+            <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
+            <div className={classes.overlay} >
+                <Typography variant="h6"> {post.creator}</Typography>
+                <Typography variant="body2"> {moment(post.createdAt).fromNow()}</Typography>
+            </div>
+            <div className={classes.overlay2} >
+                <Button style={{color: 'green'}} size="small" onClick={() => {}}>
+                    <MoreHorizIcon fontSize='default' />
+                </Button>
+            </div>
+            <div className={classes.details}>
+                <Typography variant="body2" color="textSecondary">{post.tags.map((tags) => `#${tags}  `)}</Typography>
+
+            </div>
+            <CardContent>
+            <Typography className={classes.title} variant="h5" gutterBottom>{post.message}</Typography>
+            </CardContent>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary" onClick={() => {}} >
+                    <ThumbUpAltIcon fontSize="small" />
+                    Like 
+                    {post.likeCount}
+                </Button>
+                <Button size="small" color="primary" onClick={() => {}} >
+                    <DeleteIcon fontSize="small" />
+                    Delete
+                </Button>
+            </CardActions>
+        </Card>
     );
 }
 
