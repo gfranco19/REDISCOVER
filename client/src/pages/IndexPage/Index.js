168f25580d27121f0f95701195907208c54bdea4
Giovanny Franco <gfranco19@live.com>
2021-01-13 18:59:05 -0800

    icon animation

--- client/src/pages/IndexPage/Index.js
+++ client/src/pages/IndexPage/Index.js
@@ -3,24 +3,23 @@ import "./Index.css";
 import { Button } from "react-bootstrap";
 import whitelogo from "../../assets/images/whitelogo.png"
 import { Link } from "react-router-dom";
-import NavTabs from "../../components/NavTabs/NavTabs";
+
 import Footer from "../../components/Footer/Footer"
 
 function Index() {
     return (
         <div>
-            <NavTabs />
             <div className="welcomeBackground">
                 <br /><br /><br /><br /><br />
 
-                <div><img src={whitelogo} alt="logo" id="logo" class='center' />
+                <div><img src={whitelogo} alt="logo" id="logo" className='center pulse' />
                     <br /><br />
 
                     <p class="description">Welcome to a community of urban explorers, <br />discovering the rich and historical world around us -  <br />right in our own backyards.</p></div>
 
                 <br /><br /><br />
 
-                <div class="center">
+                <div className="center">
                     <Button variant="light" size="lg" className='welcomeButton'><Link to="./Register">register</Link></Button>{' '}
                     <Button variant="light" size="lg" className='welcomeButton'><Link to="./Login">login</Link></Button>{' '}
                 </div>
