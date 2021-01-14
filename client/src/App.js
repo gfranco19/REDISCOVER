08aae8c97994471d2deb9016de8c442e1833d11a
Giovanny Franco <gfranco19@live.com>
2021-01-13 19:37:44 -0800

    explorer page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer"
-
 import Index from "./pages/IndexPage/Index";
 import Register from "./pages/Register/Register";
 import Login from "./pages/Login/Login";
@@ -11,6 +9,7 @@ import Haunted from "./pages/Haunted/Haunted";
 import Historical from "./pages/Historical/Historical";
 import Film from "./pages/Film/Film";
 import Example from "./pages/Example/Example";
+import Explore from "./pages/ExplorePage/Explore";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -19,10 +18,11 @@ function App() {
   return (
     <div>
       <Router>
-     
+          
           <Route exact path="/" component={Index} />
           <Route path="/Register" component={Register} />
           <Route path="/Login" component={Login} />
+          <Route path="/explorer" component={Explore} />
           <Route path="/haunted" component={Haunted} />
           <Route path="/historical" component={Historical} />
           <Route path="/film" component={Film} />
--- client/src/pages/IndexPage/Index.js
+++ client/src/pages/IndexPage/Index.js
@@ -3,45 +3,26 @@ import "./Index.css";
 import { Button } from "react-bootstrap";
 import whitelogo from "../../assets/images/whitelogo.png"
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-
-import Footer from "../../components/Footer/Footer"
-=======
 // import Footer from "../../components/Footer/Footer"
->>>>>>> f41098b07fb6c8b75fa7420b5bf3577d57c1cfda
 
 function Index() {
     return (
         <div>
-<<<<<<< HEAD
             <div className="welcomeBackground">
                 <br /><br /><br /><br /><br />
 
                 <div><img src={whitelogo} alt="logo" id="logo" className='center pulse' />
                     <br /><br />
-=======
-                <div className="welcomeBackground">
-                    <br /><br /><br /><br /><br />
-
-                    <div><img src={whitelogo} alt="logo" id="logo" class='center' />
-                        <br /><br />
->>>>>>> f41098b07fb6c8b75fa7420b5bf3577d57c1cfda
 
                         <p class="description">Welcome to a community of urban explorers, <br />discovering the rich and historical world around us -  <br />right in our own backyards.</p></div>
 
                     <br /><br /><br />
 
-<<<<<<< HEAD
-                <div className="center">
-                    <Button variant="light" size="lg" className='welcomeButton'><Link to="./Register">register</Link></Button>{' '}
-                    <Button variant="light" size="lg" className='welcomeButton'><Link to="./Login">login</Link></Button>{' '}
-=======
                     <div class="center">
                         <Button variant="light" size="lg" className='welcomeButton'><Link to="./Register">register</Link></Button>{' '}
                         <Button variant="light" size="lg" className='welcomeButton'><Link to="./Login">login</Link></Button>{' '}
                     </div>
                     {/* <Footer /> */}
->>>>>>> f41098b07fb6c8b75fa7420b5bf3577d57c1cfda
                 </div>
             </div>
     );
