cd91c6b98ea65222f2713606a545f0c8a597b208
chris covarrubias <chris.covarrubias40@gmail.com>
2021-01-19 22:14:42 -0800

    newest pull

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -27,7 +27,6 @@ const Form = () => {
 
     }
 
-<<<<<<< HEAD
     return(
         <Paper className={classes.paper}>
         <form autoComplete="off" no noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
@@ -41,28 +40,6 @@ const Form = () => {
         <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Remove</Button>
         </div>
         </form>
-=======
-    return (
-        <Paper class={classes.paper}>
-            <form autoComplete="off" no noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-                <Typography varient='h6'>Have a  place to share? Upload it below:</Typography>
-                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
-                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-                <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-                <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value })} />
-
-                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({base64}) => setPostData({...postData, selectedFile: base64})} />
-
-
-                    <IconLabelButtons />
-
-                    {/* <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Upload</Button>
-                    <Button variant="contained" color="secondary" size="large" onClick={clear} fullWidth>Remove</Button> */}
-
-
-                </div>
-            </form>
->>>>>>> 5e6c9c009865cdd09ca234025c75042bd0b0239d
 
         </Paper>
     );
