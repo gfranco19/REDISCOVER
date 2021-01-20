eb20de38f03f4e26ceb91b9e7d302a394c72c084
shay_daniel <shay@isom.org>
2021-01-20 08:32:50 -0800

    connecting back and front

--- client/src/pages/ExplorePage/Explore.js
+++ client/src/pages/ExplorePage/Explore.js
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import "./Explore.css";
+import ButtonGroupPages from "../../components/Buttons/ButtonGroup"
 import { useDispatch } from 'react-redux';
 import NavTabs from "../../components/NavTabs/NavTabs";
 import Posts from "../../components/Posts/Posts";
@@ -19,6 +21,15 @@ function Explore (){
     return(
         <div>
         <NavTabs />
+
+        <div className="nightBackground">
+                <br /><br /> <br />
+                <h1>Explore!</h1>
+                <br />
+                <div className="center">
+                    <ButtonGroupPages />
+                </div>
+            </div>
       
         <Container maxwidth="lg">
       
@@ -43,3 +54,57 @@ function Explore (){
 }
 
 export default Explore;
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import { useDispatch } from 'react-redux';
+// import NavTabs from "../../components/NavTabs/NavTabs";
+// import Posts from "../../components/Posts/Posts";
+// import useStyles from '../../styles';
+// import { getPosts } from '../../actions/posts';
+// import { Container, Typography, Grow, Grid } from "@material-ui/core"
+
+// function Explore (){
+
+//     const [ setCurrentId] = useState(null);
+//     const classes = useStyles();
+//     const dispatch = useDispatch();
+//     useEffect(() => {
+//         dispatch(getPosts());
+//       }, [dispatch])
+    
+
+//     return(
+//         <div>
+//         <NavTabs />
+      
+//         <Container maxwidth="lg">
+      
+//         <Typography className={classes.heading} variant="h2" align="center"><br /> </Typography>
+       
+//       <Grow in>
+//         <Container>
+//           <Grid container justify="space-between" alignItems="stretch" spacing="3" >
+
+//               <Grid item xs={12} sm={7}>
+//                 <Posts setCurrentId={setCurrentId} />
+//               </Grid>
+//               <Grid item xs={12} sm={4}>
+//                 </Grid>
+
+//           </Grid>
+//         </Container>
+//       </Grow>
+//     </Container>
+
+//         </div>
+
+//     )
+// }
+
+// export default Explore;
