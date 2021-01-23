14878beb109b9105ac303483b700a0d081a40a93
Giovanny Franco <gfranco19@live.com>
2021-01-23 10:14:04 -0800

    added name and email field to render on card when uploaded

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import useStyles from './styles';
 import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core/';
-import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';
 import { useDispatch } from 'react-redux'
-import { deletePost, likePost } from '../../../actions/posts';
+import { deletePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
@@ -21,7 +20,6 @@ const Post = ({ post, setCurrentId }) => {
         >
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
             <div className={classes.overlay} >
-                <Typography variant="h6" style={{ fontFamily: 'oswald' }}> {post.creator}</Typography>
                 <Typography variant="body2"> {moment(post.createdAt).fromNow()}</Typography>
             </div>
             <div className={classes.overlay2} >
@@ -30,12 +28,15 @@ const Post = ({ post, setCurrentId }) => {
                 </Button>
             </div>
             <div className={classes.details}>
+                <Typography variant="h6" style={{ fontFamily: 'oswald' }}> {post.user}</Typography>
                 <Typography variant="body2" color="primary" component="h2">{post.tags.map((tag) => `#${tag}  `)}</Typography>
             </div>
             <Typography className={classes.title} color="textSecondary" gutterBottom variant="h5" >{post.title}</Typography>
             <CardContent style={{ padding: '4px'}}>
                 <Typography variant="body2" color="textSecondary" component="p" style={{ color: 'black' }} >{post.message}</Typography>
-                <hr></hr></CardContent>
+                <hr></hr>
+            <Typography variant="body2" style={{ fontFamily: 'oswald' }}> {post.creator}</Typography>
+                </CardContent>
 
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" onClick={() => dispatch(deletePost(post._id))} >
